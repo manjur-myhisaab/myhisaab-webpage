40b0797ad5d8e22b47b26eaba4b6f0904485f990
Manjur-WD <mak985812@gmail.com>
2025-11-15 15:42:29 +0530

    updated

---
+++ main.js
@@ -0,0 +1,51 @@
+
+const { animate, scroll } = Motion
+animate("#logo",
+    { scale: [0.6, 1.2, 1] },
+    { ease: "circInOut", duration: 1 }
+);
+animate("#book-a-demo-btn",
+    { scale: [0.6, 1.2, 1] },
+    { ease: "circInOut", duration: 1 }
+);
+
+const tabs = document.querySelectorAll(".tab-btn");
+const content = document.getElementById("tab-content");
+
+const tabData = {
+    1: `<img src="./assets/images/business/swipe-12.webp" alt="Identify">`,
+    2: `<img src="./assets/images/business/swipe-13.webp" alt="Identify">`,
+    3: `<img src="./assets/images/business/swipe-14.webp" alt="Identify">`
+};
+
+// Initialize
+let activeTab = 1;
+content.innerHTML = tabData[1];
+
+// Add ACTIVE class
+function setActiveTab(btn) {
+    tabs.forEach(t => t.classList.remove("active"));
+    btn.classList.add("active");
+}
+
+function showTabContent(id) {
+    animate(content, { opacity: 0, x: -10 }, { duration: 0.2 }).finished.then(() => {
+        content.innerHTML = tabData[id];
+        animate(content, { opacity: 1, x: 0 }, { duration: 0.3 });
+    });
+}
+
+// Click handler
+tabs.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const id = btn.dataset.tab;
+        if (id == activeTab) return;
+
+        activeTab = id;
+        setActiveTab(btn);
+        showTabContent(id);
+    });
+});
+
+// Set default active tab visually
+setActiveTab(tabs[0]);
